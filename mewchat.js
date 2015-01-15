ff7631d6cfe7ed3baea627af69c4fa790ed80e1f
MewFree <damien.gonot@gmail.com>
2015-01-15 20:02:52 +0800

    better time

--- mewchat.js
+++ mewchat.js
@@ -7,6 +7,11 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.message.helpers({
+    hour: sentAt.getHours().toString(),
+    min: sentAt.getMinutes().toString();
+    });
+
   Template.write.events({
     "submit .new-message": function (event) {
       var text = event.target.text.value;
