9265f32444bb678b38ce63c89cb452892a8d6f0e
MewFree <damien.gonot@gmail.com>
2015-01-16 13:13:32 +0800

    adding documentation

--- mewchat.js
+++ mewchat.js
@@ -3,6 +3,10 @@ Messages = new Mongo.Collection("messages");
 if (Meteor.isClient) {
   Template.chat.helpers({
     messages: function () {
+      //we're looking for the last 25 messages, so we have to sort from
+      //older to newer, and limit the result to 25, but we still want it
+      //from newer to older (common practice in webchats) so we fetch and
+      //reverse the array!
       return Messages.find({}, {sort: {sentAt: -1}, limit: 25}).fetch().reverse();
     }
   });
@@ -10,12 +14,14 @@ if (Meteor.isClient) {
   Template.write.events({
     "submit .new-message": function (event) {
       var text = event.target.text.value;
+      //users can stay anonymous, if so, randomize a author name
       if (event.target.author.value == "") {
         var author = Math.random().toString(36).substring(7);
       } else {
         var author = event.target.author.value;
       }
-      
+
+      //if message is not blank, insert in db 
       if (text != "") {
         Messages.insert({
           text: text,
@@ -23,6 +29,7 @@ if (Meteor.isClient) {
           sentAt: new Date()
         });
       }
+      //clear the text field so the user can enter a new message
       event.target.text.value = "";
       return false;
     }
